aa9628390597f7c284de3d983cd80debf77e490f
sandilya761 <57587519+sandilya761@users.noreply.github.com>
2021-01-22 21:37:45 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -14,62 +14,39 @@ magic_square = [[8, 3, 4],
 human_list = []
 computer_list = []
 
-# check rows function checks whether the sum of the elements in a row of the game board is 15 
-
-def checkRows(game_board):
-    check = 0
+# check_win function checks and prints the winner
+def check_win(game_board):
+    # row
     for i in range(3):
-        for j in range(3):
-            if game_board[i][j] == 0:
-                return False
-            else:
-                check = check + game_board[i][j]
-        if check != 15:
-            return False    
-        check = 0
-    return True
+            if game_board[i][0] == game_board[i][1] == game_board[i][2] != 0 :
+                print("winner -", game_board[i][0])
+                return True
+    
+    # column
 
-# checkcols function checks whether the sum of the elements in a column of the game board is 15 
-
-def checkCols(game_board):
-    check = 0
+    for j in range(3):
+            if game_board[0][j] == game_board[1][j] == game_board[2][j] != 0:
+                print("winner |", game_board[0][j])
+                return True
+               
+        
+    #left diagonal
+    k = 0
     for i in range(3):
-        for j in range(3):
-            if game_board[j][i] == 0:
-                return False
-            else:
-                check = check + game_board[j][i]
-        if check != 15:
-            return False    
-        check = 0
-    return True
+         k = game_board[i][i] + k
+    if k!= 0 and k== 3*game_board[0][0]:
+        print("winner", game_board[0][0])
+        return True
 
-# chechDiagsleft function checks whether the sum of the elements in the left diagonal of the game board is 15
-
-def checkDiagsleft(game_board):
-    check = 0
+    #right diagonal
+    x=0
     for i in range(3):
-        if game_board[i][i] == 0:
-            return False
-        else:
-            check = check + game_board[i][i]
-    if check != 15:
-        return False
-    return True
-
-# checkDiagsright function whether the sum of the elements in right diagonal of the game board is 15
-
-def checkDiagsright(game_board):
-    check = 0
-    for i in range(0,3):
         j = 2 - i
-        if game_board[i][j] == 0:
-            return False
-        else:
-            check = check + game_board[i][j]
-    if check != 15:
-        return False    
-    return True
+        x = game_board[i][j] + x
+    if x!=0 and x == 3*game_board[2][0]:
+        print("winner /", game_board[2][0])
+        return True
+
 
 # toss function decides whether human starts playing first or computer  
 
